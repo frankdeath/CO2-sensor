9c42b7a3b6be9fb3794468e913fa94d9c087cf84
Kevin Peterson <frankdeath@gmail.com>
2022-03-05 22:24:33 -0600

    Try to force auto-calibration off (it keeps turning back on)

--- calibrate.py
+++ calibrate.py
@@ -9,34 +9,33 @@ import adafruit_scd30
 # starting at 50KHz
 i2c = busio.I2C(board.SCL, board.SDA, frequency=50000)
 scd = adafruit_scd30.SCD30(i2c)
+
 print("#####")
+
 # scd.temperature_offset = 10
 print("Temperature offset:", scd.temperature_offset)
-
 # scd.measurement_interval = 4
 print("Measurement interval:", scd.measurement_interval)
-
 # scd.self_calibration_enabled = True
 print("Self-calibration enabled:", scd.self_calibration_enabled)
-
 # scd.ambient_pressure = 1100
 print("Ambient Pressure:", scd.ambient_pressure)
-
 # scd.altitude = 100
 print("Altitude:", scd.altitude, "meters above sea level")
-
 # scd.forced_recalibration_reference = 409
 print("Forced recalibration reference:", scd.forced_recalibration_reference)
-
 # scd.self_calibration_enabled = False
 print("Self calibration enabled:", scd.self_calibration_enabled)
 
 print("#####")
 
+scd.self_calibration_enabled = False
 scd.forced_recalibration_reference = 400
 
 print("#####")
 
 # scd.forced_recalibration_reference = 409
 print("Forced recalibration reference:", scd.forced_recalibration_reference)
+# scd.self_calibration_enabled = False
+print("Self-calibration enabled:", scd.self_calibration_enabled)
 
