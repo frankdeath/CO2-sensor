b0d45abf0ace4304eb53a40404e641ded68c8d4a
Kevin Peterson <frankdeath@gmail.com>
2022-03-05 22:48:35 -0600

    Minor changes to calibrate.py

--- calibrate.py
+++ calibrate.py
@@ -12,25 +12,23 @@ scd = adafruit_scd30.SCD30(i2c)
 
 print("#####")
 
-# scd.temperature_offset = 10
-print("Temperature offset:", scd.temperature_offset)
 # scd.measurement_interval = 4
 print("Measurement interval:", scd.measurement_interval)
-# scd.self_calibration_enabled = True
-print("Self-calibration enabled:", scd.self_calibration_enabled)
+# scd.temperature_offset = 10
+print("Temperature offset:", scd.temperature_offset)
 # scd.ambient_pressure = 1100
 print("Ambient Pressure:", scd.ambient_pressure)
 # scd.altitude = 100
 print("Altitude:", scd.altitude, "meters above sea level")
 # scd.forced_recalibration_reference = 409
 print("Forced recalibration reference:", scd.forced_recalibration_reference)
-# scd.self_calibration_enabled = False
+# scd.self_calibration_enabled = True
 print("Self calibration enabled:", scd.self_calibration_enabled)
 
 print("#####")
 
 scd.self_calibration_enabled = False
-scd.forced_recalibration_reference = 400
+scd.forced_recalibration_reference = 409
 
 print("#####")
 
